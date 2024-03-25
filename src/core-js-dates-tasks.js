273a812332e2e29c367359d2d2612ab9466fb7c0
aleksandr <sanuyOKmaklaren0@gmail.com>
2024-03-26 03:06:40 +0500

    feat: solve 9 task

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -182,8 +182,18 @@ function formatDate(date) {
  * 12, 2023 => 10
  * 1, 2024 => 8
  */
-function getCountWeekendsInMonth(/* month, year */) {
-  throw new Error('Not implemented');
+function getCountWeekendsInMonth(month, year) {
+  let countWeekends = 0;
+  const daysInMonth = new Date(year, month, 0).getDate();
+
+  for (let day = 1; day <= daysInMonth; day += 1) {
+    const currentDay = new Date(year, month - 1, day).getDay();
+    if (currentDay === 0 || currentDay === 6) {
+      countWeekends += 1;
+    }
+  }
+
+  return countWeekends;
 }
 
 /**
