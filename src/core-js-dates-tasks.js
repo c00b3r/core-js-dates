e8a50d6b184ef0b9fdf78586d920a1c8a0cb4bce
c00b3r <sanuyOKmaklaren0>
2025-03-07 20:45:43 +0500

    test: 4rd task

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -77,15 +77,14 @@ function getDayName(date) {
  * Date('2024-02-13T00:00:00Z') => Date('2024-02-16T00:00:00Z')
  * Date('2024-02-16T00:00:00Z') => Date('2024-02-23T00:00:00Z')
  */
-function getNextFriday(/* date */) {
-  // const originalDate = new Date(date);
-  // let daysToAdd = (5 - originalDate.getUTCDay() + 7) % 7;
-  // if (daysToAdd === 0) {
-  //   daysToAdd = 7;
-  // }
-  // originalDate.setUTCDate(originalDate.getUTCDate() + daysToAdd);
-  // return originalDate;
-  throw new Error('Not implemented');
+function getNextFriday(date) {
+  const originalDate = new Date(date);
+  let daysToAdd = (5 - originalDate.getUTCDay() + 7) % 7;
+  if (daysToAdd === 0) {
+    daysToAdd = 7;
+  }
+  originalDate.setUTCDate(originalDate.getUTCDate() + daysToAdd);
+  return originalDate;
 }
 
 /**
